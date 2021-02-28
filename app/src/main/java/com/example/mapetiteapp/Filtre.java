e71bc6086b661b2530fe4d7ab02f88aaaafa30aa
Théophile YVARS <theo.yvars@gmail.com>
2021-03-01 00:05:59 +0100

    Intent activité

---
+++ app/src/main/java/com/example/mapetiteapp/Filtre.java
@@ -0,0 +1,14 @@
+package com.example.mapetiteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Filtre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtre);
+    }
+}
--- app/src/main/java/com/example/mapetiteapp/MainActivity.java
+++ app/src/main/java/com/example/mapetiteapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.mapetiteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonFiltre = findViewById(R.id.button1);
+        Button buttonLed = findViewById(R.id.button2);
+
+        buttonFiltre.setOnClickListener(this::filtre);
+        buttonLed.setOnClickListener(this::led);
+    }
+
+    /*
+    Fonction qui execute l'activité du calcul de filtre
+     */
+    void filtre(View sender){
+        Intent filtreActivity = new Intent(getApplicationContext(),Filtre.class);
+        startActivity(filtreActivity);
+    }
+
+    /*
+    Fonction qui execute l'activité du calcul de la resistance pour la led
+     */
+    void led(View sender){
+        Intent ledActivity = new Intent(getApplicationContext(),Led.class);
+        startActivity(ledActivity);
     }
 }

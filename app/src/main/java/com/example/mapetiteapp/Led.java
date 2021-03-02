b6bb9c2136c5333a4c779f0b61580eee5bf3ca2e
Théophile YVARS <theo.yvars@gmail.com>
2021-03-02 21:44:31 +0100

    ajout play store

--- app/src/main/java/com/example/mapetiteapp/Led.java
+++ app/src/main/java/com/example/mapetiteapp/Led.java
@@ -2,26 +2,54 @@ package com.example.mapetiteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Led extends AppCompatActivity {
 
     EditText input;
     EditText output;
+    int afficheR = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_led);
 
+       // TextView ledR = findViewById(R.id.affLed);
+
         input = findViewById(R.id.input);
         output = findViewById(R.id.output);
+        Log.i("output : ",output.getText().toString());
 
         Button buttonCalculRes = findViewById(R.id.calculRes);
         buttonCalculRes.setOnClickListener(this::calculRes);
+/*
+        buttonCalculRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String inputStr = input.getText().toString();
+                final float inputValue = Float.parseFloat(inputStr);
+                final String outputStr = String.format("%.2f", CalculResLed.calculResistance(inputValue));
+                output.setText(outputStr + " ohms");
+                afficheR = (int) CalculResLed.calculResistance(inputValue);
+
+                ledR.setText("Valeur : "+afficheR);
+            }
+        });
+*/
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("value",afficheR);
+        editor.apply();
     }
 
    void calculRes(View sender){
@@ -29,5 +57,8 @@ public class Led extends AppCompatActivity {
         final float inputValue = Float.parseFloat(inputStr);
         final String outputStr = String.format("%.2f", CalculResLed.calculResistance(inputValue));
         output.setText(outputStr + " ohms");
+        afficheR = (int) CalculResLed.calculResistance(inputValue);
+        TextView ledR = findViewById(R.id.affLed);
+        ledR.setText("R : "+afficheR);
    }
 }
--- app/src/main/java/com/example/mapetiteapp/MainActivity.java
+++ app/src/main/java/com/example/mapetiteapp/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.sql.BatchUpdateException;
+import android.widget.ImageButton;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton buttonPlayStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonFiltre = findViewById(R.id.button1);
-        Button buttonLed = findViewById(R.id.button2);
+        Button buttonLed = findViewById(R.id.lien);
         Button buttonAop = findViewById(R.id.button3);
 
         buttonFiltre.setOnClickListener(this::filtre);
         buttonLed.setOnClickListener(this::led);
         buttonAop.setOnClickListener(this::aop);
+
+        this.buttonPlayStore = findViewById(R.id.lienPlay);
+        this.buttonPlayStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlayStore();
+            }
+        });
     }
 
     /*
@@ -44,5 +54,13 @@ public class MainActivity extends AppCompatActivity {
     void aop(View sender){
         Intent aopActivity = new Intent(getApplicationContext(),Aop.class);
         startActivity(aopActivity);
+    }
+
+    public void  openPlayStore()  {
+        String url="https://play.google.com/store/search?q=electronique";
+
+        // An implicit intent, request a URL.
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        this.startActivity(intent);
     }
 }
